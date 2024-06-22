55e350a9324d793aa01078de7d1f06a084137ee8
Ankit Sharma <ankitkr1152@gmail.com>
2024-06-22 23:00:50 +0530

    Multiple Inheritance and some more concepts

--- OOPS/01_solution.py
+++ OOPS/01_solution.py
@@ -1,12 +1,20 @@
 # class and object
-# Q1. Create a Car class with attributes like brand and model . Then create an instance of the class 
+# Q1. Create a Car class with attributes like brand and model. Then create an instance of the class 
 class Car:
+    # class variable to keep track of the total number of cars created 
+    # Q6. Add a class variable in Car class to keep track of the total number of cars created 
+    total_car = 0
     def __init__(self, brand, model):
         # Encapsulation
         # Q4. Modify the car class to encapsulate the brand attribute, making it private and provide a getter method for it
         # self.brand = brand
         self.__brand = brand
-        self.model = model
+        # self.model = model
+
+        # Q8. Use a property decorator in the car class to make the model attribute read only 
+        # to make this variable read only 
+        self.__model = model
+        Car.total_car += 1
 
     # Polymorphism
     # Q5. Demonstrate polymorphism by defining a method fuel_type in both Car and Electric Car classes , 
@@ -20,7 +28,22 @@ class Car:
     # class method and Self
     # Q2. Add a method to the car class that displays the full name of the car (brand and model)
     def getFullName(self):
-        return f"{self.__brand} {self.model}"
+        return f"{self.__brand} {self.__model}"
+    
+    # static method :- belongs to the class rather than an instance of the class
+    # Q7. Add a static method in Car class that returns a general description of a car
+    @staticmethod
+    def generaldescription():
+        return "This is a general description of a car"
+    
+
+    # Property decorators :- allows us to create read-only attributes
+    # Q8. Use a property decorator in the car class to make the model attribute read only 
+    # to make this variable read only 
+
+    @property  # This property decorator makes the attribute read-only 
+    def model(self):
+        return self.__model
     
 
 # Inheritance
@@ -32,16 +55,29 @@ class ElectricCar(Car):
 
     def fuel_type(self):
         return "Electric Power"
+    
+
 
 myTesla = ElectricCar("Tesla","Model S", "85kwh")
+
+# Q9. Check if myTesla object is an instance of both Car and ElectricCar classes.
+print(isinstance(myTesla, Car)) 
+print(isinstance(myTesla, ElectricCar)) 
+
+
 # print(myTesla.__brand)
 print(myTesla.model, myTesla.battery_size)
 print(myTesla.getFullName())   # Output: Tesla Model S
 print(myTesla.get_brand())   # Output: Tesla
 print(myTesla.fuel_type())   # Output: Electric Power
 
+nexon = Car("Tata", "Nexon")
 mercedes = Car("Mercedes", "Benz")
+print(nexon.model) # Output: Nexon
 print(mercedes.fuel_type()) # Output: Diesel or Petrol
+print(Car.total_car) 
+
+print(Car.generaldescription()) # Output: This is a general description of a car
 
 
 
@@ -50,3 +86,19 @@ print(mercedes.fuel_type()) # Output: Diesel or Petrol
 # print(myCarObj.model)   
 # print(myCarObj.getFullName())   
 
+
+# Q10. Multiple Inheritance
+class Battery:
+    def battery_info(self):
+        return "This is a battery"
+    
+class Engine:
+    def engine_info(self):
+        return "This is an engine"
+    
+class ElectricCarTwo(Battery, Engine, Car):
+    pass
+
+my_new_tesla = ElectricCarTwo("Tesla","Model S")
+print(my_new_tesla.battery_info())
+print(my_new_tesla.engine_info())
